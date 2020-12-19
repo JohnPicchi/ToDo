f8443e2e5797d5151144c06a0c3c3b3047e53a04
John Picchi <john.picchi@gmail.com>
2020-12-19 18:29:17 -0500

    ToDo Controller changes

---
+++ ToDo.Api/Controllers/ToDoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Api.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ToDoController : ControllerBase
+  {
+    public ToDoController()
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<ToDo>> Get()
+    {
+      return null;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(Guid id)
+    {
+      return null;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create()
+    {
+      return null;
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit()
+    {
+      return null;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+      return null;
+    }
+  }
+}
---
+++ ToDo.Api/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Api
+{
+  public class ToDo
+  {
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public int Priority { get; set; }
+
+    public DateTime DateCreatedUtc { get; set; }
+
+    public DateTime DateUpdatedUtc { get; set; }
+  }
+}
